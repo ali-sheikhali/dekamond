bfa34543f17f234400baa66968e658369eda7f8f
ali-sheikhali <ali.sheikhali1998@gmail.com>
2025-06-10 18:37:32 +0330

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AuthPage() {
+  return (
+   <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+        {/* We'll add the form here later */}
+      </div>
+    </div>
+  )
+}
